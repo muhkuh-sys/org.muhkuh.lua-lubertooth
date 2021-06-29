8abc01b526b292e29987b438ef7e8f9247d5be0b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2021-06-29 17:49:51 +0200

    Initial import.

---
+++ lubertooth/installer/lua5.4/install_windows.lua
@@ -0,0 +1,11 @@
+local t = ...
+local strDistId, strDistVersion, strCpuArch = t:get_platform()
+local tResult
+
+if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
+  t:install('lua_plugins/', '${install_lua_cpath}/')
+  t:install('lua/', '${install_lua_path}/')
+  tResult = true
+end
+
+return tResult
